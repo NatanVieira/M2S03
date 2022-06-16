2e9577bf1fc1a69ddbe8437dc8babc43bb77ea3b
Natanael Vieira <natanaelvieira130@gmail.com>
2022-06-16 20:59:48 -0300

    M2S03 - Exercicio 04

--- Screens/MenuScreen.cs
+++ Screens/MenuScreen.cs
@@ -21,7 +21,8 @@ public static class MenuScreen {
                 switch (escolhaUsuario)
                 {
                     case "1":
-                        CadastrarProduto();
+                        CriarProdutoScreen.Iniciar();
+                        Iniciar();
                         break;
                     case "2":
                         Iniciar();
@@ -39,32 +40,5 @@ public static class MenuScreen {
                         Iniciar();
                         break;
                 }
-    }
-
-    public static void CadastrarProduto(){
-        string? nome = "";
-        double? qtdEstoque = 0;
-        decimal? valorCompra = 0M;
-        decimal? valorVenda = 0M;
-        
-        Console.WriteLine("Preencha as informações abaixo");
-        Console.WriteLine("Nome:");
-        nome = Console.ReadLine();
-        Console.WriteLine("Quantidade em estoque:");
-        qtdEstoque = Convert.ToDouble(Console.ReadLine());
-        Console.WriteLine("Valor de compra:");
-        valorCompra = Convert.ToDecimal(Console.ReadLine());
-        Console.WriteLine("Valor de venda:");
-        valorVenda = Convert.ToDecimal(Console.ReadLine());
-        string id = Guid.NewGuid().ToString();
-        Console.WriteLine("ID: {0}", id);
-        Console.WriteLine("Nome: {0}", nome);
-        Console.WriteLine("Qtd: {0}", qtdEstoque);
-        Console.WriteLine($"R$ Compra: {valorCompra.ToString()}");
-        Console.WriteLine($"R$ Venda: {valorVenda}");
-        Console.WriteLine("Pressione Qualquer tecla para voltar ao menu");
-        string? voltaMenu = Console.ReadLine();
-        if (voltaMenu != null && voltaMenu != "")
-            Iniciar();
     }
 }
